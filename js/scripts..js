210d619e6202bd5aec31898da26c3f521d50dab5
Acheampong Nana <achiekobby17@gmail.com>
2021-07-26 09:46:42 +0000

    Responsiveness added to landing page

--- js/scripts..js
+++ js/scripts..js
@@ -3,10 +3,23 @@ const CloseBtn = document.getElementById("close-menu");
 const NavMenu = document.getElementById("nav-menu");
 
 
-MenuBtn.addEventListener('click', ()=>{
-    NavMenu.classList.toggle("show");
+MenuBtn.addEventListener("click", () => {
+  NavMenu.classList.toggle("show");
 });
 
-CloseBtn.addEventListener("click",()=>{
-    NavMenu.classList.remove("show");
-})
+CloseBtn.addEventListener("click", () => {
+  NavMenu.classList.remove("show");
+});
+
+// SCROLL REVEAL ANIMATION
+
+const scrollReveal = ScrollReveal({
+  reset: true,
+  duration: 2000,
+  origin: "top",
+  distance: "30px",
+});
+
+scrollReveal.reveal(`.active__card,.num-card,.card__comment,.card__stats,
+
+.how__to-card,.pricing__card, .reveal`);
